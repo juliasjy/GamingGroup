42aab4f8126f18e4553e62d8c17910e0812014b4
Trigon <trigon65199@gmail.com>
2017-01-24 22:33:23 -0500

    Update JS modules

---
+++ db.js
@@ -0,0 +1,51 @@
+var mssql = require('mssql');  
+var db = {};  
+var config = {  
+  user: 'wuy8',  
+  password: 'wyl1995',  
+  server: 'titan.csse.rose-hulman.edu',   
+  database: 'NWindwuy81710',  
+  port:1433,  
+  options: {  
+    encrypt: true // Use this if you're on Windows Azure  
+  },  
+  pool: {  
+    min: 0,  
+    max: 10,  
+    idleTimeoutMillis: 3000  
+  }  
+};  
+  
+//执行sql,返回数据.  
+db.sql = function (sql, callBack) {  
+  var connection = new mssql.Connection(config, function (err) {  
+    if (err) {  
+      console.log(err);  
+      return;  
+    }  
+    var ps = new mssql.PreparedStatement(connection);  
+    ps.prepare(sql, function (err) {  
+      if (err){  
+        console.log(err);  
+        return;  
+      }  
+      ps.execute('', function (err, result) {  
+        if (err){  
+          console.log(err);  
+          return;  
+        }  
+  
+        ps.unprepare(function (err) {  
+          if (err){  
+            console.log(err);  
+            callback(err,null);  
+            return;  
+          }  
+            callBack(err, result);  
+        });  
+      });  
+    });  
+  });  
+};  
+  
+module.exports = db;  
